e679a14d9c647222a524ddab48afdcdcd621d033
fadlyfptr <fadlyfirmansyahputra@gmail.com>
2022-02-25 16:58:39 +0700

    update

--- Login/js/main.js
+++ Login/js/main.js
@@ -1,57 +1,66 @@
+// (function ($) {
+//     "use strict";
 
-(function ($) {
-    "use strict";
+//     /*==================================================================
+//     [ Validate ]*/
+//     var input = $('.validate-input .input100');
 
-    
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+//     $('.validate-form').on('submit',function(){
+//         var check = true;
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+//         for(var i=0; i<input.length; i++) {
+//             if(validate(input[i]) == false){
+//                 showValidate(input[i]);
+//                 check=false;
+//             }
+//         }
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+//         return check;
+//     });
 
-        return check;
-    });
+//     $('.validate-form .input100').each(function(){
+//         $(this).focus(function(){
+//            hideValidate(this);
+//         });
+//     });
 
+//     function validate (input) {
+//         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//                 return false;
+//             }
+//         }
+//         else {
+//             if($(input).val().trim() == ''){
+//                 return false;
+//             }
+//         }
+//     }
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
+//     function showValidate(input) {
+//         var thisAlert = $(input).parent();
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
+//         $(thisAlert).addClass('alert-validate');
+//     }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+//     function hideValidate(input) {
+//         var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+//         $(thisAlert).removeClass('alert-validate');
+//     }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+// })(jQuery);
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
+function Login() {
+  var done = 0;
+  var username = document.login.username.value;
+  username = username.toLowerCase();
+  var password = document.login.password.value;
+  password = password.toLowerCase();
+  if (username == "admin" && password == "admin") {
+    alert("Login berhasil ");
+    window.location = "homelogin.html";
+  } else if (done == 0) {
+    alert("Invalid login!");
+  }
+}
